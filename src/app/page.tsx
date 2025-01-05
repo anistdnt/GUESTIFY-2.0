71f434df78239efee74327fe446a74a7d04aa9a9
devarkabrata <arkabrata.chandra@codeclouds.co.in>
2025-01-05 22:59:09 +0530

    Header successfully added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
-// Importing Components 
+// Importing Components
 import DisplayCard from "@/components/DisplayCard/DisplayCard";
 
 export default function Home() {
   return (
-    <div className="flex flex-wrap justify-center items-center min-h-screen">
-      <DisplayCard/>
-      <DisplayCard/>
-      <DisplayCard/>
-      <DisplayCard/>
-      <DisplayCard/>
+    <div className=" grid grid-cols-12 gap-3">
+      <div className="col-span-12 md:col-span-6 lg:col-span-4">
+        <DisplayCard />
+      </div>
+      <div className="col-span-12 md:col-span-6 lg:col-span-4">
+        <DisplayCard />
+      </div>
+      <div className="col-span-12 md:col-span-6 lg:col-span-4">
+        <DisplayCard />
+      </div>
+      <div className="col-span-12 md:col-span-6 lg:col-span-4">
+        <DisplayCard />
+      </div>
+      <div className="col-span-12 md:col-span-6 lg:col-span-4">
+        <DisplayCard />
+      </div>
     </div>
   );
 }
