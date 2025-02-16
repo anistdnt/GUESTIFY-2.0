1caae5c2f7b775e9c0a22489fa5935fa35598828
MasudAlamMolla99 <masudalammolla99@gmail.com>
2025-02-16 22:19:34 +0530

    Changes in searchbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,33 +4,35 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div className="flex items-center  px-8 lg:px-16 bg-white">
-     
-      <div className="w-full lg:w-1/2  text-center lg:text-left">
-        <h1 className="text-4xl text-center lg:text-7xl font-bold text-gray-600 ">
-          Discover Your Perfect PG Stay
-        </h1>
-     
-        <p className="mt-4 text-lg lg:text-medium text-gray-600 text-center">
-          Find your ideal paying guest accommodation with ease.
-        </p>
+      <div className="flex items-center  px-8 lg:px-16 bg-white">
+        <div className="w-full lg:w-1/2  text-center lg:text-left">
+          <h1 className="text-4xl text-center lg:text-7xl font-bold text-gray-600 ">
+            Discover Your Perfect PG Stay
+          </h1>
+
+          <p className="mt-4 text-lg lg:text-medium text-gray-600 text-center">
+            Find your ideal paying guest accommodation with ease.
+          </p>
+        </div>
+
+        <div className="hidden md:block md:w-1/2 lg:block w-1/2">
+          <Image
+            src="/assets/mainImage.png"
+            alt="PG Stay"
+            width={500} // Set actual width
+            height={500} // Set actual height
+            className="w-full mb-9 "
+          />
+        </div>
       </div>
 
- 
-      <div className="hidden md:block md:w-1/2 lg:block w-1/2">
-      <Image
-          src="/assets/mainImage.png"
-          alt="PG Stay"
-          width={500} // Set actual width
-          height={500} // Set actual height
-          className="w-full mb-9 "
-          />
-      </div>
-    </div>
-  
-      <div className="mx-auto lg:w-4/5 p-4 bg-white rounded-lg shadow-lg">
+      <div className="mx-auto lg:w-3/5 p-4 bg-white rounded-lg shadow-lg">
         <div className="flex items-center border w-full border-gray-300 rounded-lg overflow-hidden">
-          <div></div>
+          <select name="" id="" className="mr-3 ml-3">
+            <option value="">College</option>
+            <option value="">College</option>
+            <option value="">College</option>
+          </select>
           <input
             type="text"
             placeholder="Search..."
@@ -41,8 +43,6 @@ export default function Home() {
           </button>
         </div>
       </div>
-   
-    
-          </>
+    </>
   );
 }
