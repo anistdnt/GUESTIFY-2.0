d68052cf5c6b6d5c1e0e851755187eef3c76c67f
devarkabrata <arkabrata.chandra@codeclouds.co.in>
2025-02-16 23:57:24 +0530

    commit

--- src/components/DisplayCard/DisplayCard.tsx
+++ src/components/DisplayCard/DisplayCard.tsx
@@ -8,38 +8,6 @@ type Props = {
 
 export default function DisplayCard({ number_of_stars }: Props) {
   return (
-    // <div className="flex flex-row justify-center items-start bg-gray-100 shadow-md m-4 px-1 pt-1 pb-3 max-w-5xl rounded-md">
-    //   <Image
-    //     src={"/assets/sample1.jpg"}
-    //     alt="House"
-    //     width={350}
-    //     height={350}
-    //     loading="eager"
-    //     className="rounded-md"
-    //   />
-    //   <div className="flex flex-col gap-2 px-2">
-    //     <div className="text-lg text-cardTitleCol font-medium font-cardTitle">
-    //       Sample PG Title
-    //     </div>
-    //     <div className="text-cardDesCol text-cardDescription text-sm">
-    //       27, Navakrishna Guie Lane , Kolkata-700010 , West Bengal, to this
-    //       occation os
-    //     </div>
-    //     <hr/>
-    //     <div className="flex flex-row justify-between">
-    //       <div className="flex flex-row justify-end items-center gap-2 text-cardTitle text-cardTitleCol font-semibold">
-    //         <CurrencyInr size={20}/>
-    //         <span>3000</span>
-    //         <span className="text-sm">onwards</span>
-    //       </div>
-    //       {/* <button className="bg-buttons px-4 rounded-md font-semibold text-white">View</button> */}
-    //     </div>
-    //     <div>
-    //       <Rating no_of_star={number_of_stars}/>
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="max-w-5xl mx-auto bg-white rounded-xl shadow-md overflow-hidden border border-gray-200">
       <div className="relative">
         <Image
