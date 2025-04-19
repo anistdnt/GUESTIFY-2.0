616f17d5f388c875d7f009d4fa85ef3f3fc34395
itzarkabrata <arkabrata1292@gmail.com>
2025-04-19 12:58:08 +0530

    notification skeleton pushed

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
+  // Check if 'device_token' cookie is already set or not
+  const existingDeviceToken = req.cookies.get("device_token");
+
+  if (!existingDeviceToken) {
+    const deviceToken = crypto.randomUUID();
+
+    const oneYearInSeconds = 60 * 60 * 24 * 365;
+
+    res.cookies.set("device_token", deviceToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: oneYearInSeconds,
+      sameSite: "lax",
+      path: "/",
+    });
+  }
+
+  return res;
+}
