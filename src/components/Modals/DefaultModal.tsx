1d9e11f730d186cc38b0626e2fa052f71420df6c
itzarkabrata <arkabrata1292@gmail.com>
2025-05-31 10:54:23 +0530

    major code update

---
+++ src/components/Modals/DefaultModal.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import OwnerInfoModal from './OwnerInfoModal'
+// import ResetPasswordModal from './ResetPasswordModal'
+
+type Props = {}
+
+export default function DefaultModal({}: Props) {
+  return (
+    <div>
+        {/* <OwnerInfoModal/> */}
+        {/* <ResetPasswordModal/> */}
+    </div>
+  )
+}
