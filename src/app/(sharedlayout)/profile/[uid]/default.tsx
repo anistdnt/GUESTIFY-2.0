2199d3fb7ceea65f7caaff6f68320b5e3a22887a
Aniruddha Mukherjee <ani05jeet.2003@gmail.com>
2025-09-02 22:00:31 +0530

    pg min rent show is dynamic now , data refetch for pg and room delete implemented

--- src/app/(sharedlayout)/profile/[uid]/default.tsx
+++ src/app/(sharedlayout)/profile/[uid]/default.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { FaTimes } from "react-icons/fa";
 import Link from "next/link";
