df8c402b0370eeb6fd1312972913a42394c0f0be
itzarkabrata <arkabrata1292@gmail.com>
2025-10-05 21:42:40 +0530

    User, Admin Segregated

--- src/app/(sharedlayout)/profile/[uid]/page.tsx
+++ src/app/(sharedlayout)/profile/[uid]/page.tsx
@@ -58,10 +58,10 @@ export default function Profile() {
             </p>
           </div>
           <div className="flex items-center space-x-4">
-            <i
+            {/* <i
               onClick={handleModal}
               className="fa-solid fa-magnifying-glass text-black/50 sm:mr-4 cursor-pointer"
-            ></i>
+            ></i> */}
             <div className="relative w-12 h-12">
               <Image
                 src={
@@ -86,18 +86,21 @@ export default function Profile() {
           >
             Profile
           </Link>
-          <Link
+          
+          {profile_info?.is_admin && <Link
             href={`/profile/${uid}/mypg`}
             className={linkClass(`/profile/${uid}/mypg`)}
           >
             My Enlisted PGs
-          </Link>
-          <Link
+          </Link>}
+
+          {profile_info?.is_admin && <Link
             href={`/profile/${uid}/stats`}
             className={linkClass(`/profile/${uid}/stats`)}
           >
             Statistics
-          </Link>
+          </Link>}
+          
         </div>
 
         {isModalOpen && (
